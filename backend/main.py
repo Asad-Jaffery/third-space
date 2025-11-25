8f119626b56decab853d588bfc6d2402bc60bdc8
Asad-Jaffery <asadmjaffery@gmail.com>
2025-11-25 12:25:23 -0800

    feat: reviews endpoint + schema

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from routes import users, spaces
+from routes import users, spaces, reviews
 
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(spaces.router)
+app.include_router(reviews.router)
 
 
 # basic activity check
